170f76a108b9258cc12b8d5d55c1a7c5f12eab53
C3NZ <cenz@cenz.io>
2018-12-03 20:26:59 -0800

    [feat] make sure that the user is authorized to create a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,32 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
+const checkAuth = require('../lib/authorizeUser');
+
 //Get the form for creating a new post
 router.get('/new', (req, res) => {
-    res.render('new-post', {});
+    res.render('new-post', res.locals);
 })
 
 //GET the view for a specific post
 router.get('/:postId', (req, res) => {
     Post.findOne({_id: req.params.postId}).populate('comments').then(post => {
-        res.render('show-post', {post: post})
+        res.locals.post = post 
+        res.render('show-post', res.locals)
     }).catch(err => {
         console.error(err);
     })
 })
 
-//Create a new post
+//POST a new post to geddit
 router.post('/', (req, res) => {
+    checkAuth(res.locals.user); 
+    
     let post = new Post(req.body);
-    
-    post.save((err, post) => {
-        if(err) {
-            console.error(err);
-        }else{
-            return res.redirect(`/`);
-        }
-    })
-    
+        post.save((err, post) => {
+            if(err) {
+                console.error(err);
+            }else{
+                return res.redirect(`/`);
+            }
+        })
 })
 
 module.exports = router;

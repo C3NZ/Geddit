f160caa231a88e3f5e4490ba0d8a22d8a11d1ce7
C3NZ <cenz@cenz.io>
2018-12-05 02:21:59 -0800

    [feat] add profiles router to handle profile pages

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+const Post = require('../models/post')
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', (req, res) => {
+    User
+        .findOne({ username: req.params.username })
+        .then((user) => {
+            if (user) {
+                res.locals.profile = user;
+                return Post.find({ author: user._id });
+            }
+            return res.status(404).send('Profile was not found!');
+        })
+        .then((posts) => {
+            res.locals.posts = posts;
+            return res.render('profile', res.locals);
+        })
+        .catch(err => res.status(500).send(err.message));
 });
 
 module.exports = router;

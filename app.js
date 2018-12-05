3b1502b670962c0d62e9d95da35e4acf3a9fe28d
C3NZ <cenz@cenz.io>
2018-12-05 02:17:23 -0800

    [fix] fix indentation

--- app.js
+++ app.js
@@ -39,24 +39,24 @@ app.use('/', checkAuth);
 // Our defined routes
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/users', usersRouter);
+app.use('/profiles', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
